6c0f7ce6db3409d67faef80a7f435c69effd820d
mlloydbixal <mandy.lloyd@bixal.com>
2023-06-29 20:38:03 -0400

    VOTE-1624: remove hard coded content with json data

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -1,42 +1,36 @@
 import { useState } from 'react';
 import { Button } from '@trussworks/react-uswds';
+import content from "../data/step-three.json";
 
 function StepThree(props) {
-    const [buttonOneStyle, setbuttonOneStyle] = useState(true)
-    const [buttonTwoStyle, setbuttonTwoStyle] = useState(true)
-    const [buttonSelected, setButtonSelected] = useState()
-
-    // const styleOne = buttonSelected === 'update' ? true : false;
-    // const styleTwo = buttonSelected === 'new' ? true : false;
+    const [buttonSelected, setButtonSelected] = useState('no selection')
 
     const handleClick = (id) => {
         if (id === 'update') {
-            // setbuttonOneStyle(true);
-            // setbuttonTwoStyle(false);
             setButtonSelected('update')
         } else if (id === 'new') {
-            // setbuttonTwoStyle(true);
-            // setbuttonOneStyle(false);
             setButtonSelected('new')
         }
     }
 
+    const test = content.heading_one.replace("%state_name%", props.stateData.name);
+
     return (
         <>
-        <h1>Let’s get started with your {props.stateData.name} voting registration</h1>
-        <p>Choose your path to update your registration or to begin a new registration. You’ll be taken to a digital registration form to answer a few questions. This form should take you between 5 to 10 minutes.</p>
-        <h2>Select one of the options below to continue.</h2>
-        <p>Use this path to update your registration information due to a change of name or change of address, or to register with a political party. </p>
+        <h1>{content.heading_one.replace("%state_name%", props.stateData.name)}</h1>
+        <p>{content.subheading_one}</p>
+        <h2>{content.heading_two}</h2>
+        <p>{content.help_text_one}</p>
         <Button id="update" onClick={e => {{props.getRegPath(e.target.id)}; handleClick(e.target.id);}} outline={buttonSelected === 'update' ? false : true}>
-            I'd like to update my registration
+            {content.button_update_reg}
         </Button>
-        <p>Use this path to register for the first time or to register in a new state. As a first time voter in your location, you are legally required to show proof of identification when you first vote. We recommend you have a driver’s license or non-driver identification number available for registration.</p>
+        <p>{content.help_text_two}</p>
         <Button id="new" onClick={e => {{props.getRegPath(e.target.id)}; handleClick(e.target.id);}} outline={buttonSelected === 'new' ? false : true}>
-            I'd like to register for the first time
+        {content.button_update_reg}
         </Button>
         <div className="button-container" style={{ margin:'20px' }}>
-            <Button type="button" onClick={props.handleNext}>
-            Continue to the digital form
+            <Button type="button" onClick={props.handleNext} disabled={buttonSelected === 'no selection' ? true : false}>
+            {content.button_continue}
             </Button>
         </div>
         </>
