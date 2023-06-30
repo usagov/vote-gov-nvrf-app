28cf5a1515384a7c27085d8502a83bc1d309b68c
SamiraMSadat <samira.sadat@gsa.gov>
2023-06-30 09:49:32 -0600

    VOTE-1625 Add general form text and autocomplete off to fields

--- src/components/StepFour.jsx
+++ src/components/StepFour.jsx
@@ -2,6 +2,7 @@
 import { Form, Label, TextInput, Button, Dropdown,Checkbox, DatePicker } from '@trussworks/react-uswds';
 import StateSelector from './StateSelector';
 import React, { useState } from "react";
+import content from "../data/step-four.json";
 
 function StepFour(props) {
 
@@ -82,11 +83,13 @@ function StepFour(props) {
     //Form section variables
     const personalInfoSection =
         <>
+        <h4>{content.personal_info_heading}</h4>
+        <p>{content.personal_info_text}</p>
+
         {nameVisible && (
             <div>
-                <h4>Personal Information</h4>
                 <Label htmlFor="title-select">Title</Label>
-                <Dropdown id="title-select" name="title-select">
+                <Dropdown id="title-select" name="title-select" autocomplete="off">
                     <option>- Select -{' '}</option>
                     <option value="Mr.">Mr.</option>
                     <option value="Miss">Miss</option>
@@ -95,16 +98,16 @@ function StepFour(props) {
                 </Dropdown>
 
                 <Label htmlFor="first-name">First Name</Label>
-                <TextInput id="first-name" name="first-name" type="text" required={nameReq}/>
+                <TextInput id="first-name" name="first-name" type="text" autocomplete="off" required={nameReq}/>
 
                 <Label htmlFor="middle-name">Middle Name</Label>
-                <TextInput id="middle-name" name="middle-name" type="text"/>
+                <TextInput id="middle-name" name="middle-name" type="text" autocomplete="off"/>
 
                 <Label htmlFor="last-name">Last Name</Label>
-                <TextInput id="last-name" name="last-name" type="text" required={nameReq}/>
+                <TextInput id="last-name" name="last-name" type="text" autocomplete="off" required={nameReq}/>
 
                 <Label htmlFor="suffix-select">Suffix</Label>
-                <Dropdown id="suffix-select" name="suffix-select" required={nameReq}>
+                <Dropdown id="suffix-select" name="suffix-select" autocomplete="off" required={nameReq}>
                     <option>- Select -{' '}</option>
                     <option value="Jr.">Jr.</option>
                     <option value="Sr.">Sr.</option>
@@ -123,7 +126,7 @@ function StepFour(props) {
             <div value={previousName} onChange={onChangePreviousName}>
                 <h4>Previous Name</h4>
                 <Label htmlFor="title-select-2">Title</Label>
-                <Dropdown id="title-select-2" name="title-select-2">
+                <Dropdown id="title-select-2" name="title-select-2" autocomplete="off">
                     <option>- Select -{' '}</option>
                     <option value="Mr.">Mr.</option>
                     <option value="Miss">Miss</option>
@@ -132,16 +135,16 @@ function StepFour(props) {
                 </Dropdown>
 
                 <Label htmlFor="first-name-2">First Name</Label>
-                <TextInput id="first-name-2" name="first-name-2" type="text" required={nameReq}/>
+                <TextInput id="first-name-2" name="first-name-2" type="text" autocomplete="off" required={nameReq}/>
 
                 <Label htmlFor="middle-name-2">Middle Name</Label>
-                <TextInput id="middle-name-2" name="middle-name-2" type="text" required={nameReq}/>
+                <TextInput id="middle-name-2" name="middle-name-2" type="text" autocomplete="off" required={nameReq}/>
 
                 <Label htmlFor="last-name-2">Last Name</Label>
-                <TextInput id="last-name-2" name="last-name-2" type="text" required={nameReq}/>
+                <TextInput id="last-name-2" name="last-name-2" type="text" autocomplete="off" required={nameReq}/>
 
                 <Label htmlFor="suffix-select-2">Suffix</Label>
-                <Dropdown id="suffix-select-2" name="suffix-select-2" required={nameReq}>
+                <Dropdown id="suffix-select-2" name="suffix-select-2" autocomplete="off" required={nameReq}>
                     <option>- Select -{' '}</option>
                     <option value="Jr.">Jr.</option>
                     <option value="Sr.">Sr.</option>
@@ -158,28 +161,28 @@ function StepFour(props) {
                 <div className="usa-hint" id="date-of-birth-hint">
                     mm/dd/yyyy
                 </div>
-                <DatePicker aria-describedby="date-of-birth-hint" aria-labelledby="date-of-birth-label" id="date-of-birth" name="date-of-birth" required={dobReq}/>
+                <DatePicker aria-describedby="date-of-birth-hint" aria-labelledby="date-of-birth-label" id="date-of-birth" name="date-of-birth" autocomplete="off" required={dobReq}/>
             </div>
         )}
 
         {telephoneVisible && (
             <div>
                 <Label htmlFor="phone-number">Phone Number (123-456-7890)</Label>
-                <TextInput id="phone-number" name="phone-number" type="text" required={telephoneReq}/>
+                <TextInput id="phone-number" name="phone-number" type="text" autocomplete="off" required={telephoneReq}/>
             </div>
         )}
 
         {partyVisible && (
             <div>
                 <Label htmlFor="political-party">Choice of Party</Label>
-                <TextInput id="political-party" name="political party" type="text" required={partyReq}/>
+                <TextInput id="political-party" name="political party" type="text" autocomplete="off" required={partyReq}/>
             </div>
         )}
 
         {raceVisible && (
             <div>
                 <Label htmlFor="race-ethic-group-select">Race</Label>
-                <Dropdown id="race-ethic-group-select" name="race-ethic-group-select" required={raceReq}>
+                <Dropdown id="race-ethic-group-select" name="race-ethic-group-select" autocomplete="off" required={raceReq}>
                     <option>- Select -{' '}</option>
                     <option value="American Indian or Alaska Native">American Indian or Alaska Native</option>
                     <option value="Asian or Pacific Islander">Asian or Pacific Islander</option>
@@ -199,19 +202,19 @@ function StepFour(props) {
             <div>
                 <h4>Home Address</h4>
                 <Label htmlFor="street-address">Street Address</Label>
-                <TextInput id="street-address" name="street-address" type="text" required={addressReq}/>
+                <TextInput id="street-address" name="street-address" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="apt-num">Apartment of Lot #</Label>
-                <TextInput id="apt-num" name="apt-num" type="text"/>
+                <TextInput id="apt-num" name="apt-num" type="text" autocomplete="off"/>
 
                 <Label htmlFor="city">City</Label>
-                <TextInput id="city" name="city" type="text" required={addressReq}/>
+                <TextInput id="city" name="city" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label>State</Label>
-                <StateSelector id="state" statesList={props.statesList} required={addressReq}/>
+                <StateSelector id="state" statesList={props.statesList} autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="zipcode">Zipcode (123456)</Label>
-                <TextInput id="zipcode" name="zip-code" type="text" required={addressReq}/>
+                <TextInput id="zipcode" name="zip-code" type="text" autocomplete="off" required={addressReq}/>
 
                 { changeRegistrationVisible && (
                     <div>
@@ -226,19 +229,19 @@ function StepFour(props) {
             <div value={previousAddress} onChange={onChangePreviousAddress}>
                 <h4>Previous Address</h4>
                 <Label htmlFor="street-address-3">Street Address</Label>
-                <TextInput id="street-address-3" name="street-address-3" type="text" required={addressReq}/>
+                <TextInput id="street-address-3" name="street-address-3" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="apt-num-3">Apartment of Lot #</Label>
-                <TextInput id="apt-num-3" name="apt-num-3" type="text"/>
+                <TextInput id="apt-num-3" name="apt-num-3" type="text" autocomplete="off"/>
 
                 <Label htmlFor="city-3">City</Label>
-                <TextInput id="city-3" name="city-3" type="text" required={addressReq}/>
+                <TextInput id="city-3" name="city-3" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label>State</Label>
-                <StateSelector id="state-3" statesList={props.statesList} required={addressReq}/>
+                <StateSelector id="state-3" statesList={props.statesList} autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="zipcode-3">Zipcode (123456)</Label>
-                <TextInput id="zipcode-3" name="zip-code-3" type="text" required={addressReq}/>
+                <TextInput id="zipcode-3" name="zip-code-3" type="text" autocomplete="off" required={addressReq}/>
             </div>
         )}
 
@@ -246,19 +249,19 @@ function StepFour(props) {
             <div value={mailAddress} onChange={onChangeMailAddress}>
                 <h4>Mailing Address</h4>
                 <Label htmlFor="street-address-2">Street Address</Label>
-                <TextInput id="street-address-2" name="street-address-2" type="text" required={addressReq}/>
+                <TextInput id="street-address-2" name="street-address-2" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="apt-num-2">Apartment of Lot #</Label>
-                <TextInput id="apt-num-2" name="apt-num-2" type="text"/>
+                <TextInput id="apt-num-2" name="apt-num-2" type="text" autocomplete="off"/>
 
                 <Label htmlFor="city-2">City</Label>
-                <TextInput id="city-2" name="city-2" type="text" required={addressReq}/>
+                <TextInput id="city-2" name="city-2" type="text" autocomplete="off" required={addressReq}/>
 
                 <Label>State</Label>
-                <StateSelector id="state-2" statesList={props.statesList} required={addressReq}/>
+                <StateSelector id="state-2" statesList={props.statesList} autocomplete="off" required={addressReq}/>
 
                 <Label htmlFor="zipcode-2">Zipcode (123456)</Label>
-                <TextInput id="zipcode-2" name="zip-code-2" type="text" required={addressReq}/>
+                <TextInput id="zipcode-2" name="zip-code-2" type="text" autocomplete="off" required={addressReq}/>
             </div>
         )}
     </>
@@ -269,7 +272,7 @@ function StepFour(props) {
             <div>
                 <h4>Identification</h4>
                 <Label htmlFor="state-id-num">State Driver's License Number</Label>
-                <TextInput id="state-id-num" name="state-id-num" type="text" required={idNumReq}/>
+                <TextInput id="state-id-num" name="state-id-num" type="text" autocomplete="off" required={idNumReq}/>
 
                 <Label htmlFor="issue-date" id="issue-date-label">Issue Date</Label>
                 <div className="usa-hint" id="issue-date-hint">
@@ -288,15 +291,20 @@ function StepFour(props) {
     return (
         <>
         {/* uswds components, html, jsx output goes here*/}
+        <h1>{content.main_heading}</h1>
+        <p>{content.intro_text}</p>
+
         <Form onSubmit={(e) => {handleSubmit(e)}}>
             {step === 1 && personalInfoSection}
             {step === 2 && addressSection}
             {step === 3 && identificationSection}
 
             <div className="button-container" style={{ margin:'20px' }}>
-                <Button type="button" onClick={handlePrev}>
-                    Previous
-                </Button>
+                {(step > 1 ) && (
+                    <Button type="button" onClick={handlePrev}>
+                        Previous
+                    </Button>
+                )}
                 {(step < 3) && (
                     <Button type="button" onClick={handleNext}>
                         Next
