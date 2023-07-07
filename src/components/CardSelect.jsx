598b2bdc94fb91cc6cb9212a8ddff99c78a3759b
mlloydbixal <mandy.lloyd@bixal.com>
2023-07-07 13:38:18 -0400

    VOTE-1691: create card buttons

---
+++ src/components/CardSelect.jsx
@@ -0,0 +1,18 @@
+import styles from "../styles/CardSelect.module.css";
+
+function CardSelect(props) {
+    return (
+        <>
+        <div className={styles[props.cardStyle]}>
+            <div className={styles['card-contents']}>
+            <div>
+                <img src={props.iconPath}></img>
+            </div>
+            <div className={styles['text']}>{props.text}</div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default CardSelect;
