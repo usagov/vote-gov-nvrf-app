e9a971bb74b14761a0b681780fae4804f1c9ddbc
SamiraMSadat <samira.sadat@gsa.gov>
2023-07-10 11:37:18 -0600

    VOTE-1688 Disable current state field and use value from first state dropdown

--- src/components/StateSelector.jsx
+++ src/components/StateSelector.jsx
@@ -3,7 +3,7 @@ import { Dropdown } from '@trussworks/react-uswds';
 function StateSelector(props) {
     return (
         <>
-        <Dropdown id="input-dropdown" name="input-dropdown" value={props.state} onChange={props.saveState}>
+        <Dropdown id="input-dropdown" name="input-dropdown" value={props.state} onChange={props.saveState} disabled={props.disabled}>
             <option>Select your home State or Territory </option>
             {props.statesList.map(
             state => <option key={state} value={state}>{state}</option>
