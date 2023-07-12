8ef646a54e0d884f64f4ccbafe9e0aa4c77a3a43
SamiraMSadat <samira.sadat@gsa.gov>
2023-07-12 11:37:47 -0600

    VOTE-1628 Add content and submit button to delivery type components

--- src/components/DeliveryOptions/Email.jsx
+++ src/components/DeliveryOptions/Email.jsx
@@ -1,7 +1,22 @@
+import { Label, TextInput, Button } from '@trussworks/react-uswds';
+
 function Email(props) {
     return (
         <>
-        <p>Email option displays here</p>
+        <h3>Enter email address</h3>
+        <p>Please enter a valid address. For example, name@mail.com</p>
+
+        <Label htmlFor="email-address">Email Address</Label>
+        <TextInput id="email-address" name="email-address" value={props.fieldData.email_address} onChange={props.saveFieldData('email_address')} type="text" autoComplete="off" required={true}/>
+
+        <h3>Would you like a reminder to mail your form?</h3>
+        <p>Sign up here for reminders. By entering your phone number, you consent to receiving messages from vote.gov.</p>
+        <Label htmlFor="sms-alert-phone-number">Opt in for SMS Alerts (optional)</Label>
+        <TextInput id="sms-alert-phone-number" name="sms-alert-phone-number" value={props.fieldData.sms_alert_phone_number} onChange={props.saveFieldData('sms_alert_phone_number')} type="text" autoComplete="off" required={false}/>
+
+        <Button type="submit" onClick={props.handleNext}>
+            Send Form to My Email Address
+        </Button>
         </>
     );
 }
