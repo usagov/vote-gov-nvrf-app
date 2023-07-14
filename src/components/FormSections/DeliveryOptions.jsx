8af7d10b6e9d5c9327fbe69795d7aa036d8e40ff
SamiraMSadat <samira.sadat@gsa.gov>
2023-07-14 11:25:40 -0600

    VOTE-1628 Create components for success page based on the delivery option

--- src/components/FormSections/DeliveryOptions.jsx
+++ src/components/FormSections/DeliveryOptions.jsx
@@ -6,15 +6,6 @@ import content from "../../data/registration-form.json";
 import CardSelect from '../CardSelect';
 
 function DeliveryOptions(props){
-    const [buttonSelected, setButtonSelected] = useState('email')
-
-    const handleClick = (deliveryType) => {
-        if (deliveryType === 'email') {
-            setButtonSelected('email')
-        } else if (deliveryType === 'print') {
-            setButtonSelected('print')
-        }
-    }
 
     return (
         <>
@@ -22,46 +13,46 @@ function DeliveryOptions(props){
         <p>You can generate a printable document (.pdf) of your completed form and print now, or have a printable document sent to your email address to print later.</p>
 
         <h3>Select one of the options below to continue.</h3>
-        <div onClick={() => {handleClick('email')}}>
-        <CardSelect 
+        <div onClick={() => {props.handleClickDeliveryButton('email')}}>
+        <CardSelect
             iconPath={"/images/email-print-mail.svg"}
-            text={"Email, print, and mail"} 
-            cardStyle={buttonSelected === 'email' ? 'card-selected' : 'card'}
+            text={"Email, print, and mail"}
+            cardStyle={props.deliveryButtonSelected === 'email' ? 'card-selected' : 'card'}
         />
         </div >
-        <div onClick={() => {handleClick('print')}}>
-        <CardSelect 
+        <div onClick={() => {props.handleClickDeliveryButton('print')}}>
+        <CardSelect
             iconPath={"/images/email-print-mail.svg"}
-            text={"Print"} 
-            cardStyle={buttonSelected === 'print' ? 'card-selected' : 'card'}
+            text={"Print"}
+            cardStyle={props.deliveryButtonSelected === 'print' ? 'card-selected' : 'card'}
         />
         </div >
 
-        {buttonSelected === 'email' &&
+        {props.deliveryButtonSelected === 'email' &&
             <Email
                 state={props.state}
                 stateData={props.stateData}
                 fieldData={props.fieldData}
                 saveFieldData = {props.saveFieldData}
-                deliveryOption = {buttonSelected}
+                deliveryOption = {props.deliveryButtonSelected}
                 handleNext={props.handleNext}
                 buttonDisabled={props.buttonDisabled}
             />
         }
 
-        {buttonSelected === 'print' &&
+        {props.deliveryButtonSelected === 'print' &&
             <Print
                 state={props.state}
                 stateData={props.stateData}
                 fieldData={props.fieldData}
                 saveFieldData = {props.saveFieldData}
-                deliveryOption = {buttonSelected}
+                deliveryOption = {props.deliveryButtonSelected}
                 handleNext={props.handleNext}
                 buttonDisabled={props.buttonDisabled}
             />
         }
         <Button type="submit" onClick={props.handleNext}>
-            {buttonSelected === 'email' ? 'Send Form to My Email Address' : 'Print'}
+            {props.deliveryButtonSelected === 'email' ? 'Send Form to My Email Address' : 'Print'}
         </Button>
         </>
     );
--- src/components/FormSections/SuccessPage.jsx
+++ src/components/FormSections/SuccessPage.jsx
@@ -1,32 +1,42 @@
 import ProgressBar from '../ProgressBar';
 import { Button } from '@trussworks/react-uswds';
+import EmailSuccess from '../DeliveryOptions/EmailSuccess';
+import PrintSuccess from '../DeliveryOptions/PrintSuccess';
 
 function SuccessPage(props) {
+    const deliveryOption = props.deliveryButtonSelected;
+
     return (
         <>
-        <h1>Success! Your form has been sent to your email.</h1>
-        <p>Check your email to make sure you have received a PDF of your form. Be sure to print out the completed form, put it in an envelope, add the correct postage, and mail it to your state election office.</p>
-        <h3>Create a reminder to mail your form</h3>
-        <p>Take a minute and set a reminder for yourself to make sure you don’t forget to mail in your form — add it to your calendar or jot it down on a notepad and put it on your fridge to help you remember.</p>
+            {(deliveryOption === 'email') &&
+                <EmailSuccess/>
+            }
 
-        <p><strong>Mail to the following address:</strong></p>
-        <p>Division of Elections State of Alaska</p>
-        <p>PO Box 110017</p>
-        <p>Juneau, AK 99811-0017</p>
+            {(deliveryOption === 'print') &&
+                <PrintSuccess/>
+            }
 
-        <p>You may also deliver the application in person to your local voter registration office.</p>
+            <h3>Create a reminder to mail your form</h3>
+            <p>Take a minute and set a reminder for yourself to make sure you don’t forget to mail in your form — add it to your calendar or jot it down on a notepad and put it on your fridge to help you remember.</p>
 
-        <p><strong>First time voter requirements</strong></p>
-        <p>If you are registering to vote for the first time in your jurisdiction and are mailing this registration application, Federal law requires you to show proof of identification the first time you vote. Proof of identification includes: </p>
-        <ul>
-            <li>A current and valid photo identification or </li>
-            <li>A current utility bill, bank statement, government check, paycheck or government document that shows your name and address. </li>
-        </ul>
+            <p><strong>Mail to the following address:</strong></p>
+            <p>Division of Elections State of Alaska</p>
+            <p>PO Box 110017</p>
+            <p>Juneau, AK 99811-0017</p>
 
-        <p>Voters may be exempt from this requirement if they submit a COPY of this identification with their mail in voter registration form. If you wish to submit a COPY, please keep the following in mind:• Your state may have additional identification requirements which may mandate you showidentification at the polling place even if you meet the Federal proof of identification.• Do not submit original documents with this application, only COPIES</p>
+            <p>You may also deliver the application in person to your local voter registration office.</p>
 
-        <h3>Don’t forget to vote!</h3>
-        <p>Prepare to cast your vote. Explore Vote.gov to learn more about how our elections are run and your voting options.</p> 
+            <p><strong>First time voter requirements</strong></p>
+            <p>If you are registering to vote for the first time in your jurisdiction and are mailing this registration application, Federal law requires you to show proof of identification the first time you vote. Proof of identification includes: </p>
+            <ul>
+                <li>A current and valid photo identification or </li>
+                <li>A current utility bill, bank statement, government check, paycheck or government document that shows your name and address. </li>
+            </ul>
+
+            <p>Voters may be exempt from this requirement if they submit a COPY of this identification with their mail in voter registration form. If you wish to submit a COPY, please keep the following in mind:• Your state may have additional identification requirements which may mandate you showidentification at the polling place even if you meet the Federal proof of identification.• Do not submit original documents with this application, only COPIES</p>
+
+            <h3>Don’t forget to vote!</h3>
+            <p>Prepare to cast your vote. Explore Vote.gov to learn more about how our elections are run and your voting options.</p> 
 
             <a href="https://vote.gov">
                 <Button type="button" onClick={props.handlePrev}>
