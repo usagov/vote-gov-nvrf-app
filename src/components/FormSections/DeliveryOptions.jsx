b98ca14e59dfb305002ca0e2bceb257a14e9f9fb
SamiraMSadat <samira.sadat@gsa.gov>
2023-08-09 16:00:42 -0600

    VOTE-1851 Install PDF lib and dependencies, generate pdf with a few data fields

--- src/components/FormSections/DeliveryOptions.jsx
+++ src/components/FormSections/DeliveryOptions.jsx
@@ -2,12 +2,61 @@ import { Button, Grid, GridContainer } from '@trussworks/react-uswds';
 import React, { useState } from "react";
 import Email from "../DeliveryOptions/Email";
 import Print from "../DeliveryOptions/Print";
-import content from "../../data/registration-form.json";
 import CardSelect from '../CardSelect';
 import styles from "../../styles/DeliveryOptions.module.css";
+import { PDFDocument} from 'pdf-lib';
+import download from "downloadjs";
+
+async function fillForm(formData) {
+    // Fetch the PDF with form fields
+    const formUrl = 'https://www.eac.gov/sites/default/files/eac_assets/1/6/Federal_Voter_Registration_ENG.pdf'
+    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+
+    // Load a PDF with form fields
+    const pdfDoc = await PDFDocument.load(formPdfBytes)
+
+    // Get the form containing all the fields
+    const form = pdfDoc.getForm()
+
+    //Optional - print the names of all form fields
+    const fields = form.getFields()
+    fields.forEach(field => {
+    const type = field.constructor.name
+    const name = field.getName()
+    console.log(`${type}: ${name}`)
+    })
+
+    // Get all fields in the PDF by their names
+    const title =  form.getRadioGroup('salutation');
+    const title2 = form.getRadioGroup('salutation_2')
+    const firstName = form.getTextField('First Name');
+    const secondSuffix = form.getCheckBox('II');
+
+    //Get all field values in the web form by their names - this is already in props!
+    console.log(formData);
+
+    // Fill in the pdf fields
+    firstName.setText(formData.first_name);
+    // Logic for title select has to be seperated into two radio groups
+    if (formData.title == 'Mr' || formData.title == "Mrs"){
+        title.select(formData.title);
+    } else {
+        title2.select(formData.title);
+    }
+
+    //Dropdown to checkbox logic for suffix
+    if (formData.suffix === 'II') {
+        secondSuffix.check();
+    }
+
+    // Serialize the PDFDocument to bytes (a Uint8Array)
+    const pdfBytes = await pdfDoc.save()
+
+    // Trigger the browser to download the PDF document
+    download(pdfBytes, "pdf-lib_form_creation_example.pdf", "application/pdf");
+}
 
 function DeliveryOptions(props){
-
     return (
         <>
         <h2>You're Almost Done!</h2>
@@ -26,7 +75,7 @@ function DeliveryOptions(props){
                             cardStyle={props.deliveryButtonSelected === 'email' ? 'card-selected' : 'card'}/>
                         </div>
                     </Grid>
-                </Grid>                    
+                </Grid>
                 </div>
                 <div className={styles['card-padding']}>
                 <Grid row>
@@ -38,7 +87,7 @@ function DeliveryOptions(props){
                                 cardStyle={props.deliveryButtonSelected === 'print' ? 'card-selected' : 'card'}/>
                         </div>
                     </Grid>
-                </Grid>                    
+                </Grid>
                 </div>
             </Grid>
         </GridContainer>
@@ -66,7 +115,9 @@ function DeliveryOptions(props){
                 buttonDisabled={props.buttonDisabled}
             />
         }
-        <Button type="submit">
+        {/* On submit, generate a PDF, download or email based on delivery option */}
+
+        <Button onClick={ async () => {await fillForm(props.fieldData);} } type="submit">
             {props.deliveryButtonSelected === 'email' ? 'Send Form to My Email Address' : 'Print'}
         </Button>
         </>
