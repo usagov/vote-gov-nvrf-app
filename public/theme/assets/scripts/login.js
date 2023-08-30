f6022747a57a725cf0d0a2a830a9d7c1659af4d5
Ray Estrada <ray.estrada@bixal.com>
2023-08-30 16:07:25 -0700

    Enable env vars configuration

--- public/theme/assets/scripts/login.js
+++ public/theme/assets/scripts/login.js
@@ -1,41 +1,40 @@
+import { USER, PASS } from './config.js'
 let id = (id) => document.getElementById(id);
 let user = id("loginUser"),
-  pass = id("loginPass"),
-  confirm = id("loginConfirm"),
-  error = id("loginError"),
-  dialog = id("loginDialog"),
-  container = id("protoContainer");
+    pass = id("loginPass"),
+    confirm = id("loginConfirm"),
+    error = id("loginError"),
+    dialog = id("loginDialog"),
+    container = id("protoContainer");
 
-const loginCred = {
-  user: 'dm90ZS1nb3YtcHJvdG90eXBl',
-  pass: 'cGFzc3dvcmQ='
-};
+if (USER.length && PASS.length) {
+  const loginCred = {
+    user: USER,
+    pass: PASS
+  };
 
-dialog.showModal();
+  dialog.showModal();
 
-confirm.addEventListener("click", (event) => {
-  event.preventDefault();
-  validate();
-});
+  confirm.addEventListener("click", (event) => {
+    event.preventDefault();
+    validate();
+  });
 
-const validate = () => {
-  let testPass = btoa(pass.value) === loginCred.pass;
-  let testUser = btoa(user.value) === loginCred.user;
+  const validate = () => {
+    let testPass = btoa(pass.value) === loginCred.pass;
+    let testUser = btoa(user.value) === loginCred.user;
 
-  if (testPass && testUser) {
-    success();
-  } else {
-    error.removeAttribute("hidden");
+    if (testPass && testUser) {
+      success();
+    } else {
+      error.removeAttribute("hidden");
+    }
   }
-}
 
-const success = () => {
-  dialog.close();
+  const success = () => {
+    dialog.close();
+    container.removeAttribute("hidden");
+  }
+} else {
   container.removeAttribute("hidden");
-}
-
-// Allow developers to disable login by setting data attribute on dialog element.
-let status = dialog.getAttribute('data-enabled');
-if (status == "false") {
-  success();
 }
