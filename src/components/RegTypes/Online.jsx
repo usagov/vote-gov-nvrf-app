3e2dacedaddc5443644fad19ea2c358f11a31cbc
mlloydbixal <mandy.lloyd@bixal.com>
2023-09-04 19:03:16 -0400

    add blur event to parent of checkboxes

--- src/components/RegTypes/Online.jsx
+++ src/components/RegTypes/Online.jsx
@@ -1,11 +1,38 @@
-import { Button, Radio, Icon } from '@trussworks/react-uswds';
+import React, { useState, useEffect } from "react";
+import { Button, Fieldset, Checkbox, Icon, Label } from '@trussworks/react-uswds';
 import data from "../../data/state-selection.json";
+import validationStyles from "../../styles/ValidationStyles.module.css";
 
 function Online(props) {
     const content = data;
     const onlineContent = data.online;
     const stateContent = props.stateData;
     const stateLink = props.stateData.election_website_url;
+
+    const [handleErrors, setHandleErrors] = useState({ 
+        citizen: null,
+        age: null,
+        allBoxesChecked: null
+    })
+
+    const [boxValues, setBoxValues] = useState(null)
+
+    useEffect(() => {
+        setBoxValues([handleErrors.citizen, handleErrors.age])
+      }, [handleErrors]);
+
+
+    const handleCheckbox = (checked, name) => { 
+        setHandleErrors({ ...handleErrors, [name]: (checked) })
+}
+
+    const checkBoxValues = () => {
+        if (boxValues.includes(false || null)) {
+            setHandleErrors({ ...handleErrors, allBoxesChecked: (false) })
+        } else {
+            setHandleErrors({ ...handleErrors, allBoxesChecked: (true) })
+        }
+     }
 
     return (
         <>
@@ -39,53 +66,50 @@ function Online(props) {
         <h2>{content.heading_mail}</h2>
         <p>{onlineContent.mail_more_info}</p>
 
-        <h3>{content.heading_confirm}</h3>
-
-        <p>{content.citizen_required}</p>
-        <form>
-        <Radio 
-            id="yes-citizen" 
-            name="input-radio" 
-            label="Yes" 
-            onClick={e => props.handleRadio(e.target.id)} 
-            checked={props.radioValid.citizen === true ? true : false}
-            readOnly
-        />
-        <Radio 
-            id="no-citizen" 
-            name="input-radio" 
-            label="No" 
-            onClick={e => props.handleRadio(e.target.id)}
-            readOnly
-        />            
-        </form>
-
-        <p>{content.age_required}</p>
-        <form>
-        <Radio 
-            id="yes-age" 
-            name="input-radio" 
-            label="Yes" 
-            onClick={e => props.handleRadio(e.target.id)} 
-            checked={props.radioValid.age === true ? true : false}
-            readOnly
-        />
-        <Radio 
-            id="no-age" 
-            name="input-radio" 
-            label="No" 
-            onClick={e => props.handleRadio(e.target.id)}
-            readOnly
-        />            
-        </form>
+        <form onSubmit={() => {checkBoxValues(), props.handleNext()}}>
+        <Fieldset legend="Historical figures 1" legendStyle="srOnly">
+            <div tabIndex={0} className={validationStyles[handleErrors.allBoxesChecked === false && 'error-container']}>            
+            <Label htmlFor="eligibility-error" id="eligibility-error">
+                {content.heading_confirm}
+            </Label>
+                <div id="test-id" tabIndex={0} onBlur={(e) => { if (!e.currentTarget.contains(e.relatedTarget)) checkBoxValues(); }}>
+                    <Checkbox
+                        id="citizen-checkbox"
+                        name="eligibility-checkbox"
+                        value="citizen-checkbox"
+                        label={content.citizen_required}
+                        aria-required="true"
+                        required={true}
+                        tabIndex={0}
+                        onClick={(e) => handleCheckbox(e.target.checked, 'citizen')}
+                    />              
+                    <Checkbox
+                        id="age"
+                        name="eligibility-checkbox"
+                        value="age-checkbox"
+                        label={content.age_required}
+                        aria-required="true"
+                        required={true}
+                        tabIndex={0}
+                        onClick={(e) => handleCheckbox(e.target.checked, 'age')}
+                    />
+                </div>
+            {handleErrors.allBoxesChecked === false && 
+                <span id="phone-number-error" rol="alert" className={validationStyles['error-text']}>
+                    Both boxes must be checked to continue.
+                </span>
+            }  
+            </div>
+        </Fieldset>
 
         <p>If you checked "No" in response to either of these questions, do not continue with registration on Vote.gov.</p>
 
         <div className="button-container" style={{ margin:'20px' }}>
-            <Button type="button" onClick={props.handleNext} disabled={props.buttonDisabled ? false : true}>
+            <Button type="submit">
             {onlineContent.start_button}
             </Button>
         </div>
+        </form>
         </>
     );
 }
