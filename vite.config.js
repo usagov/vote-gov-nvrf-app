9216956da5c73b0f2f95a5263813b6af75961c8a
SamiraMSadat <samira.sadat@gsa.gov>
2023-09-06 09:01:23 -0600

    Revert "VOTE-65 Add local pdf, start debugging fs module"
    This reverts commit b0fdf7aaea369e79c5662df84eefff2e28477746.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-      include: ['fs/promises'],
-      // Whether to polyfill specific globals.
-      /*globals: {
-        Buffer: true, // can also be 'build', 'dev', or false
-        global: true,
-        process: true,
-      },*/
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    })],
+  plugins: [react()],
   base: './',
   build: {
     outDir: '_site'
