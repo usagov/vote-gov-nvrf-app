fb05eaaa16058f5f7712c775bc538bf53939c560
Christian Medders <christian.medders@gsa.gov>
2023-10-17 12:05:10 -0400

    Vote-320: updated commands to to help check download

--- testing/cypress/support/commands.js
+++ testing/cypress/support/commands.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 Cypress.Commands.add('signin', (username, password) => {
 
-  cy.visit('/')
+  cy.visit('/', {
+    onBeforeLoad (win) {
+      cy.stub(win, 'open').as('open')
+    }
+  })
   cy.get('[id="loginUser"]').type(username)
   cy.get('[id="loginPass"]').type(password)
 
