75fb32ee525e7d1cd700983c7179320b0051c412
SamiraMSadat <samira.sadat@gsa.gov>
2023-10-30 15:59:02 -0600

    VOTE-332 Move registration form json to public folder

--- src/components/HelperFunctions/JsonHelper.jsx
+++ src/components/HelperFunctions/JsonHelper.jsx
@@ -1,6 +1,6 @@
 export const fetchData = async(filename,setContent) => {
     const locale = document.documentElement.lang;
-    const path = `src/data/${locale}/${filename}`;
+    const path = `data/${locale}/${filename}`;
     console.log(path);
     const response = await fetch(path).then(response => response.json());
     setContent(response);
