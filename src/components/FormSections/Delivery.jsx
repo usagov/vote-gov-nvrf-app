9f3a3aa2e7da630d34bf3c8d4cf0ffe6cb09be82
Ray Estrada <raymond.estrada@gsa.gov>
2023-12-21 09:21:44 -0800

    VOTE-632 add data load condition to app.jsx and remove from subsequent steps VOTE-635 update data to remove html entities VOTE-634 add required indicator to the identification heading

--- src/components/FormSections/Delivery.jsx
+++ src/components/FormSections/Delivery.jsx
@@ -1,4 +1,4 @@
-import { Button, Link, Icon, Grid } from '@trussworks/react-uswds';
+import { Button, Icon, Grid } from '@trussworks/react-uswds';
 import GenerateFilledPDF from '../GenerateFilledPDF';
 import DOMPurify from 'dompurify';
 
@@ -23,15 +23,7 @@ function Delivery(props) {
         const mailingAddress = DOMPurify.sanitize(state.mailing_address_inst);
         const deliveryBody = DOMPurify.sanitize(delivery.body.replace("@state_name", props.stateData.name));
         const deliveryBodyParts = deliveryBody.split('@mailing_address_inst');
-        /*const reminderMessage = randomProperty(content.reminder_messages);
-
-        const usagov_resource_link = reactStringReplace(
-            content.usagov_rescources,
-            '%link%',
-            (match, i) => <Link key={i} href={'https://www.usa.gov/how-to-vote'} variant="external" rel="noreferrer" target="_blank">
-                {content.usagov_resources_link}
-            </Link>
-        );*/
+        // const reminderMessage = randomProperty(content.reminder_messages);
 
         const iconCheckmark = <svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M17.3497 23.0564L16.9974 22.7041L16.6439 23.0552L15.2239 24.4652L14.8678 24.8187L15.2226 25.1736L20.8126 30.7636L21.1662 31.1171L21.5197 30.7636L33.5197 18.7636L33.8733 18.41L33.5197 18.0564L32.1097 16.6464L31.7563 16.2931L31.4028 16.6463L21.1663 26.8731L17.3497 23.0564Z" fill="#5CBF72" stroke="#5CBF72"/>
@@ -56,7 +48,6 @@ function Delivery(props) {
                 </Button>
 
                 <div className={'usa-prose margin-top-4'} dangerouslySetInnerHTML= {{__html: deliveryBodyParts[1]}}/>
-
             </>
         );
     }
