308c3b074dd706c03a5239a33e8551792f572f0a
Ray Estrada <raymond.estrada@gsa.gov>
2023-12-29 09:28:09 -0800

    VOTE-636 wrap state links in conditional logic

--- src/components/RegType/ByMail.jsx
+++ src/components/RegType/ByMail.jsx
@@ -8,40 +8,44 @@ function ByMail(props) {
     const navContent = props.navContent;
     const stateContent = props.stateData;
 
-    if (content && navContent) {
-        const contentBody = DOMPurify.sanitize(content.body).replace("@state_name", stateContent.name);
-        console.log(contentBody);
-        const stateLinks = () => (
-            <div className="padding-bottom-3 padding-top-1">
-                <a href={stateContent.confirm_reg_url} className="usa-button" target="_blank">
-                    {"Check your registration"}
-                    <Icon.Launch title="External link opens new window"/>
-                </a>
-            </div>
-        );
-        const stateMailinLink = () => (
-            <p>
-                <a href={stateContent.mail_reg_url} className="text-primary" target="_blank">
-                    <strong className="text-primary underline-primary">{"Go to %state_name%'s mail-in form".replace("%state_name%", props.stateData.name)}
-                        <Icon.Launch title="External link opens new window"/></strong>
-                </a>
-            </p>
-        );
-        let contentBodyProcessed = contentBody.replace("@state_confirm_link", renderToStaticMarkup(stateLinks()));
-        contentBodyProcessed = contentBodyProcessed.replace("@state_mailin_link", renderToStaticMarkup(stateMailinLink()));
+    const contentBody = DOMPurify.sanitize(content.body).replace("@state_name", stateContent.name);
+    const stateLinks = () => (
+        <>
+            {stateContent.confirm_reg_url &&
+                <p>
+                    <a href={stateContent.confirm_reg_url} className="usa-button" target="_blank">
+                        {"Check your registration"}
+                        <Icon.Launch title="External link opens new window"/>
+                    </a>
+                </p>
+            }
+        </>
+    );
+    const stateMailinLink = () => (
+        <>
+            {stateContent.mail_reg_url &&
+                <p>
+                    <a href={stateContent.mail_reg_url} className="text-primary" target="_blank">
+                        <strong className="text-primary underline-primary">{"Go to %state_name%'s mail-in form".replace("%state_name%", props.stateData.name)}
+                            <Icon.Launch title="External link opens new window"/></strong>
+                    </a>
+                </p>
+            }
+        </>
+    );
 
-        return (
-            <>
+    let contentBodyProcessed = contentBody.replace("@state_confirm_link", renderToStaticMarkup(stateLinks()));
+    contentBodyProcessed = contentBodyProcessed.replace("@state_mailin_link", renderToStaticMarkup(stateMailinLink()));
 
-                <h1>{content.title.replace("@state_name", stateContent.name)}</h1>
+    return (
+        <>
+            <h1>{content.title.replace("@state_name", stateContent.name)}</h1>
 
-                <div className={'usa-prose'} dangerouslySetInnerHTML= {{__html: contentBodyProcessed}}/>
+            <div className={'usa-prose'} dangerouslySetInnerHTML= {{__html: contentBodyProcessed}}/>
 
-                <NextButton type={'submit'} onClick={props.handleNext} text={navContent.next.continue}/>
-
-            </>
-        );
-    }
+            <NextButton type={'submit'} onClick={props.handleNext} text={navContent.next.continue}/>
+        </>
+    );
 }
 
 export default ByMail;
