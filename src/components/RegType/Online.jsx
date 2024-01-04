ade70fc22d6bb758cde0400edbec7d1be59f009f
SamiraMSadat <samira.sadat@gsa.gov>
2024-01-04 16:10:20 -0700

    VOTE-623 Update page data and Online template

--- src/components/RegType/Online.jsx
+++ src/components/RegType/Online.jsx
@@ -10,22 +10,16 @@ function Online(props) {
 
     if (content && navContent) {
         const contentBody = DOMPurify.sanitize(content.body).replace("@state_name", stateContent.name);
-        const stateLinks = () => (
-            <>
+
+        const stateOnlineLink = () => (
                 <div className="padding-top-3 padding-bottom-1">
                     <a href={stateContent.registration_url} className="usa-button" target="_blank">
                         {"Go to %state_name%'s online form".replace("%state_name%", stateContent.name)}
                         <Icon.Launch title="External link opens new window"/>
                     </a>
                 </div>
-                <div className="padding-bottom-3 padding-top-1">
-                    <a href={stateContent.election_website_url} className="usa-button" target="_blank">
-                        Check your registration
-                        <Icon.Launch title="External link opens new window"/>
-                    </a>
-                </div>
-            </>
         );
+
         const stateMailinLink = () => (
             <p>
                 <Link href={stateContent.download_form} className="text-primary" target="_blank">
@@ -34,16 +28,29 @@ function Online(props) {
                 </Link>
             </p>
         );
-        let contentBodyProcessed = contentBody.replace("@state_links", renderToStaticMarkup(stateLinks()));
-        contentBodyProcessed = contentBodyProcessed.replace("@state_mailin_link", renderToStaticMarkup(stateMailinLink()))
+
+        const nvrfMailLink = () => (
+            <NextButton type={'submit'} onClick={props.handleNext} text={navContent.next.start}/>
+        );
+
+        const checkRegLink = () => (
+            <div className="padding-bottom-3 padding-top-1">
+                    <a href={stateContent.election_website_url} className="usa-button" target="_blank">
+                        Check your registration
+                        <Icon.Launch title="External link opens new window"/>
+                    </a>
+            </div>
+        );
+
+        let contentBodyProcessed = contentBody.replace("@state_online_link", renderToStaticMarkup(stateOnlineLink()))
+                                              .replace("@state_mailin_link", renderToStaticMarkup(stateMailinLink()))
+                                              .replace("@vote_nvrf_link", renderToStaticMarkup(nvrfMailLink()))
+                                              .replace("@state_confirm_link", renderToStaticMarkup(checkRegLink()));
 
         return (
             <>
             <h1>{content.title.replace("@state_name", stateContent.name)}</h1>
             <div className={'usa-prose'} dangerouslySetInnerHTML= {{__html: contentBodyProcessed}}/>
-
-            <NextButton type={'submit'} onClick={props.handleNext} text={navContent.next.start}/>
-
             </>
         );
     }
