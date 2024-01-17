ac8042d12da3fa0dd06164bd0c031e2eb989caa7
SamiraMSadat <samira.sadat@gsa.gov>
2024-01-17 10:37:01 -0700

    VOTE-642 Add aria-live region to the progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -12,16 +12,16 @@ function ProgressBar(props) {
       }
 
     return (
-        <>
-        <StepIndicator centered className="margin-top-6" headingLevel="h2">
-            <StepIndicatorStep label={props.content.step_label_1} status={stepProgress(1)}/>
-            <StepIndicatorStep label={props.content.step_label_2} status={stepProgress(2)}/>
-            <StepIndicatorStep label={props.content.step_label_3} status={stepProgress(3)}/>
-            <StepIndicatorStep label={props.content.step_label_4} status={stepProgress(4)}/>
-            <StepIndicatorStep label={props.content.step_label_5} status={stepProgress(5)}/>   
-            <StepIndicatorStep label={props.content.step_label_6} status={stepProgress(6)}/>   
-        </StepIndicator>        
-        </>
+        <div aria-live="polite" aria-atomic="true">
+          <StepIndicator centered className="margin-top-4" headingLevel="h2">
+              <StepIndicatorStep label={props.content.step_label_1} status={stepProgress(1)}/>
+              <StepIndicatorStep label={props.content.step_label_2} status={stepProgress(2)}/>
+              <StepIndicatorStep label={props.content.step_label_3} status={stepProgress(3)}/>
+              <StepIndicatorStep label={props.content.step_label_4} status={stepProgress(4)}/>
+              <StepIndicatorStep label={props.content.step_label_5} status={stepProgress(5)}/>
+              <StepIndicatorStep label={props.content.step_label_6} status={stepProgress(6)}/>
+          </StepIndicator>
+        </div>
     );
 }
 
