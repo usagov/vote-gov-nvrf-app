ff6609a0b21641ded444d4ad153e367647636f4c
SamiraMSadat <108487505+SamiraMSadat@users.noreply.github.com>
2024-01-24 16:49:07 -0700

    VOTE-758 Add new registration options type (#156)

---
+++ src/components/RegType/OnlineNoStateMail.jsx
@@ -0,0 +1,47 @@
+import { Link, Icon } from '@trussworks/react-uswds';
+import NextButton from '../NextButton';
+import {renderToStaticMarkup} from "react-dom/server";
+import {sanitizeDOM} from "../HelperFunctions/JsonHelper";
+
+function OnlineNoStateMail(props) {
+    const content = props.content;
+    const navContent = props.navContent;
+    const stateContent = props.stateData;
+
+    if (content && navContent) {
+        const contentBody = sanitizeDOM(content.body).replaceAll("@state_name", stateContent.name);
+        const contentBodyParts = contentBody.split("@vote_nvrf_link");
+
+        const stateOnlineLink = () => (
+                <p>
+                    <a href={stateContent.registration_url} className="usa-button" target="_blank">
+                        <span>{"Go to %state_name%'s online form".replace("%state_name%", stateContent.name)}</span>
+                        <Icon.Launch title="External link opens new window" style={{margin: "-3px -3px -3px 4px"}}/>
+                    </a>
+                </p>
+        );
+
+        const checkRegLink = () => (
+            <p>
+                    <a href={stateContent.election_website_url} className="usa-button" target="_blank">
+                        <span>Check your registration</span>
+                        <Icon.Launch title="External link opens new window" style={{margin: "-3px -3px -3px 4px"}}/>
+                    </a>
+            </p>
+        );
+
+        let contentBodyPartOne = contentBodyParts[0].replace("@state_online_link", renderToStaticMarkup(stateOnlineLink()));
+        let contentBodyPartTwo = contentBodyParts[1].replace("@state_confirm_link", renderToStaticMarkup(checkRegLink()));
+
+    return (
+        <>
+            <h1>{content.title.replace("@state_name", stateContent.name)}</h1>
+            <div className={'usa-prose'} dangerouslySetInnerHTML= {{__html: contentBodyPartOne}}/>
+            <p><NextButton noMarginTop type={'submit'} onClick={props.handleNext} text={navContent.next.start}/></p>
+            <div className={'usa-prose'} style={{marginTop: "3rem"}} dangerouslySetInnerHTML= {{__html: contentBodyPartTwo}}/>
+        </>
+        );
+    }
+}
+
+export default OnlineNoStateMail;
--- src/components/RegistrationOptions.jsx
+++ src/components/RegistrationOptions.jsx
@@ -1,6 +1,7 @@
 import ByMail from "./RegType/ByMail";
 import ByMailStateOnly from "./RegType/ByMailStateOnly";
 import Online from "./RegType/Online";
+import OnlineNoStateMail from "./RegType/OnlineNoStateMail";
 import OnlineOnly from "./RegType/OnlineOnly";
 import OnlineNoNVRF from "./RegType/OnlineNoNvrf";
 import NotNeeded from "./RegType/NotNeeded";
@@ -16,6 +17,7 @@ function RegistrationOptions(props) {
 
     if (content && navContent) {
         const onlineContent = content.find(item => item.uuid === "086a212d-4f75-47e8-aad6-24eadc4a559f");
+        const onlineNoStateMailContent = content.find(item => item.uuid === "54aab86d-419f-45ec-947e-414490ddfb87");
         const onlineOnlyContent = content.find(item => item.uuid === "216b8cd7-69e3-4947-aa2c-09dca7392a46");
         const onlineNoNvrfContent = content.find(item => item.uuid === "dea1c47d-5b08-4235-9035-7063e5fab041");
         const mailContent = content.find(item => item.uuid === "295db8a2-6df6-4c81-825d-8d03064550f9");
@@ -28,9 +30,16 @@ function RegistrationOptions(props) {
 
             <BackButton type={'button'} onClick={props.handlePrev} text={navContent.back.select_state}/>
             <GridContainer containerSize={'tablet'} className={['usa-prose', 'margin-top-5']}>
-                {(regType === 'online' && acceptsNVRF === '1') &&
+                {(regType === 'online' && acceptsNVRF === '1' && props.stateData.mail_reg_url != "") &&
                     <Online
                         content={onlineContent}
+                        navContent={props.navContent}
+                        stateData={props.stateData}
+                        handleNext={props.handleNext}
+                    />}
+                {(regType === 'online' && acceptsNVRF === '1' && props.stateData.mail_reg_url === "") &&
+                    <OnlineNoStateMail
+                        content={onlineNoStateMailContent}
                         navContent={props.navContent}
                         stateData={props.stateData}
                         handleNext={props.handleNext}
