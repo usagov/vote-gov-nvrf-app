65015a9faabbaf62ea32094d0026295ba60d86a6
clmedders <christian.medders@gsa.gov>
2024-01-25 14:48:45 -0500

    Vote-693: Added plugin for checking strings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,9 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "detect-hard-code"],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "detect-hard-code/detect-hard-code": 'warn'
   },
 }
