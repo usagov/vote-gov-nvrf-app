887e0c1a08a2e4912a4b5e5be4abf872f8cf8897
clmedders <christian.medders@gsa.gov>
2024-01-29 13:53:36 -0500

    Vote-693: Removed un needed eslint plug in

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,9 +8,8 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', "detect-hard-code"],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    "detect-hard-code/detect-hard-code": 'warn'
   },
 }
