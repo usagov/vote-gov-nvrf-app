2a7830c907865b000707ffb34387b8d337bcd9b4
WilliamYKwon <william.kwon@gsa.gov>
2024-07-16 15:39:54 -0400

    implemented FieldContainer for current zip

--- src/components/FormSections/Addresses.jsx
+++ src/components/FormSections/Addresses.jsx
@@ -1,5 +1,6 @@
 import { Label, TextInput, Checkbox, Grid } from '@trussworks/react-uswds';
 import StateSelector from '../StateSelector';
+import FieldContainer from '../FieldContainer';
 import React, { useState } from "react";
 import { restrictType, checkForErrors, toggleError } from '../HelperFunctions/ValidateField';
 import { sanitizeDOM } from '../HelperFunctions/JsonHelper';
@@ -171,34 +172,15 @@ function Addresses(props){
                         </Grid>
 
                         <Grid tablet={{ col: 3 }}>
-                        <div className="input-parent">
-                            <Label className="text-bold" htmlFor="zip-code">
-                                {zipcodeField.label} {(addressFieldsState.required === "1") && <span className={'required-text'}>*</span>}
-                            </Label>
-                            <span className="usa-hint" id="zip-hint">{zipcodeField.help_text}</span>
-                            <TextInput
-                                data-test="zip"
-                                id="zip-code"
-                                className="radius-md"
-                                aria-describedby="zip-code_error"
-                                name="zip-code"
-                                type="text"
-                                inputMode="numeric"
-                                autoComplete="off"
-                                required={(parseInt(addressFieldsState.required))}
-                                minLength={5}
-                                maxLength={5}
-                                value={props.fieldData.zip_code}
-                                onChange={props.saveFieldData('zip_code')}
-                                onKeyDown={(e) => restrictType(e, 'number')}
-                                onBlur={(e) => toggleError(e, checkForErrors(e, 'check value length'))}
-                                onInvalid={(e) => e.target.setCustomValidity(' ')}
-                                onInput={(e) => e.target.setCustomValidity('')}
-                            />
-                            <span id="zip-code_error" role="alert" className='error-text' data-test="errorText">
-                                {zipcodeField.error_msg}
-                            </span>
-                        </div>
+                            <FieldContainer
+                                fieldType={'text'} inputData={{
+                                id: zipcodeField.nvrf_id,
+                                dataTest: 'zip',
+                                required: addressFieldsState.required,
+                                label: zipcodeField.label,
+                                error_msg: zipcodeField.error_msg,
+                                help_text: zipcodeField.help_text,
+                            }} saveFieldData={props.saveFieldData} fieldData={props.fieldData}/>
                         </Grid>
                     </Grid>
                     <Checkbox data-test="checkBox" className="margin-top-3" id="alt-mail-addr" name="alt-mail-addr" checked={props.hasMailAddress} onChange={props.onChangeMailAddressCheckbox} label={differentMailAddressField.label} />
