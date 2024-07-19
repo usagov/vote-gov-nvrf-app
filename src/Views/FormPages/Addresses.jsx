ce549ccde32d34f88aafa4f19c24134a9c085ce2
WilliamYKwon <william.kwon@gsa.gov>
2024-07-19 10:28:16 -0400

    VOTE-2309: implemented FieldContainer for current apartment number (#232)

--- src/Views/FormPages/Addresses.jsx
+++ src/Views/FormPages/Addresses.jsx
@@ -1,5 +1,6 @@
 import { Label, TextInput, Checkbox, Grid } from '@trussworks/react-uswds';
 import StateSelector from 'Components/StateSelector';
+import CurrentApartmentNumber from 'Components/Fields/CurrentApartmentNumber';
 import React, { useState } from "react";
 import { restrictType, checkForErrors, toggleError } from 'Utils/ValidateField';
 import { sanitizeDOM } from 'Utils/JsonHelper';
@@ -16,7 +17,6 @@ function Addresses(props){
     //Drupal field data
     const homeAddressSectionField = fields.find(item => item.uuid === "63552bb6-6afb-46e1-8148-860242917a22");
     const streetAddressField = fields.find(item => item.uuid === "6dcb9e8c-b40a-4cda-ba5c-06b98c3375f4");
-    const aptField = fields.find(item => item.uuid === "deba9b54-68ad-4ef1-8fb5-ee34e4ab8a49");
     const cityField = fields.find(item => item.uuid === "7e39a528-7518-40cb-b7b6-b635864dc117");
     const stateField = fields.find(item => item.uuid === "fe3a2a1d-34bd-472b-a843-3fa0635c4f40");
     const zipcodeField = fields.find(item => item.uuid === "cdb06542-0cbd-4aa3-897f-83377b8d65e5");
@@ -104,22 +104,7 @@ function Addresses(props){
 
                     <Grid row gap>
                         <Grid tablet={{ col: 5}}>
-                        <Label className="text-bold" htmlFor="apartment-number">
-                            {aptField.label}
-                        </Label>
-                        <TextInput
-                            data-test="aptNumber"
-                            id="apartment-number"
-                            className="radius-md"
-                            name="apartment-number"
-                            aria-describedby=""
-                            type="text"
-                            autoComplete="off"
-                            value={props.fieldData.apt_num}
-                            onChange={props.saveFieldData('apt_num')}
-                            onInvalid={(e) => e.target.setCustomValidity(' ')}
-                            onInput={(e) => e.target.setCustomValidity('')}
-                        />
+                            <CurrentApartmentNumber {...props} />
                         </Grid>
                     </Grid>
 
