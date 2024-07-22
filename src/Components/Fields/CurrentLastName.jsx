f96f72ffe6152c2db580259c2fa25003d4fffcee
Robert Koch <robert_koch@brown.edu>
2024-07-22 10:17:13 -0400

    add field container for last name

---
+++ src/Components/Fields/CurrentLastName.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import FieldContainer from 'Components/FieldContainer';
+import {getField} from "Utils/fieldParser";
+
+function CurrentLastName(props){
+    const uuid = "b306238a-a0f6-4bb8-b8ea-b3216ca75e0b";
+    const field = getField(props.fieldContent, uuid);
+    const stateField = getField(props.stateData.nvrf_fields, field.uuid);
+
+    return (
+        <FieldContainer
+            fieldType={'text'} inputData={{
+            id: field.nvrf_id,
+            dataTest: 'LastName',
+            required: stateField.required,
+            label: field.label,
+            error_msg: field.error_msg,
+            help_text: field.help_text,
+        }} saveFieldData={props.saveFieldData} fieldData={props.fieldData}/>
+    )
+}
+
+export default CurrentLastName;
