a05cd4d46eb349274c0cca79ec5c5553cf07c85a
WilliamYKwon <william.kwon@gsa.gov>
2024-07-25 16:14:48 -0400

    code clean (removed unused variables)

--- src/Views/FormPages/Addresses.jsx
+++ src/Views/FormPages/Addresses.jsx
@@ -23,8 +23,6 @@ function Addresses(props){
     //Drupal field data
     const homeAddressSectionField = fields.find(item => item.uuid === "63552bb6-6afb-46e1-8148-860242917a22");
     const streetAddressField = fields.find(item => item.uuid === "6dcb9e8c-b40a-4cda-ba5c-06b98c3375f4");
-    const cityField = fields.find(item => item.uuid === "7e39a528-7518-40cb-b7b6-b635864dc117");
-    const stateField = fields.find(item => item.uuid === "fe3a2a1d-34bd-472b-a843-3fa0635c4f40");
     const zipcodeField = fields.find(item => item.uuid === "cdb06542-0cbd-4aa3-897f-83377b8d65e5");
 
     const prevAddressSectionField = fields.find(item => item.uuid === "023fda0f-e8bd-4654-ab5c-46f44a0b7bd6");
