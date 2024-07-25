c94672c4c0338d2ba9e5e89970178f6080d94ded
WilliamYKwon <william.kwon@gsa.gov>
2024-07-25 14:36:50 -0400

    code clean

--- src/Components/FieldComponents/TextInputField.jsx
+++ src/Components/FieldComponents/TextInputField.jsx
@@ -8,7 +8,7 @@ function TextInputField({ inputData, saveFieldData, fieldData }){
         data-test={inputData.dataTest}
         id={inputData.id}
         className="radius-md"
-        aria-describedby={inputData.describedby || `${inputData.id}` + '_error'}
+        aria-describedby={`${inputData.id}` + '_error'}
         name={inputData.id}
         type="text"
         autoComplete="off"
@@ -18,8 +18,8 @@ function TextInputField({ inputData, saveFieldData, fieldData }){
         value={fieldData[inputData.id]}
         onChange={saveFieldData(inputData.id)}
         onBlur={(e) => toggleError(e, checkForErrors(e, inputData.check || 'check value exists'))}
-        onInvalid={(e) => {e.target.setCustomValidity(' '), inputData.pointToError && inputData.pointToError()}}
-        onInput={(e) => {e.target.setCustomValidity(''), inputData.pointToHint && inputData.pointToHint()}}
+        onInvalid={(e) => e.target.setCustomValidity(' ')}
+        onInput={(e) => e.target.setCustomValidity('')}
         />
     )
 }
--- src/Components/Fields/CurrentPhoneNumber.jsx
+++ src/Components/Fields/CurrentPhoneNumber.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import FieldContainer from 'Components/FieldContainer';
 import {getField} from "Utils/fieldParser";
 
@@ -6,15 +6,6 @@ function CurrentPhoneNumber(props){
     const uuid = "2d61b54a-e568-410f-825a-0ca82dfd3f63";
     const field = getField(props.fieldContent, uuid);
     const stateField = getField(props.stateData.nvrf_fields, field.uuid);
-
-    const [describedby, setDescribedby] = useState('phone_number-hint');
-
-    const pointToHint = () => {
-        setDescribedby('phone_number-hint');
-    }
-    const pointToError = () => {
-        setDescribedby('phone_number_error');
-    }
 
     return (
         <FieldContainer
@@ -28,9 +19,6 @@ function CurrentPhoneNumber(props){
             maxLength: 14,
             minLength: 14,
             check: 'check value length',
-            describedby: describedby,
-            pointToHint: pointToHint,
-            pointToError: pointToError,
         }} saveFieldData={props.saveFieldData} fieldData={props.fieldData} />
     )
 }
