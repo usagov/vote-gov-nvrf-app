79a816778a7752c49c88e2b064dbd52dc244ac5f
WilliamYKwon <william.kwon@gsa.gov>
2024-07-31 15:26:08 -0400

    VOTE-2322: implemented FieldContainer for mailing zipcode (#257)

--- src/Views/FormPages/Addresses.jsx
+++ src/Views/FormPages/Addresses.jsx
@@ -5,6 +5,7 @@ import CurrentStreetAddress from 'Components/Fields/CurrentStreetAddress';
 import CurrentZipCode from 'Components/Fields/CurrentZipCode';
 import CurrentCity from 'Components/Fields/CurrentCity';
 import CurrentApartmentNumber from 'Components/Fields/CurrentApartmentNumber';
+import MailingZipCode from 'Components/Fields/MailingZipCode';
 import PreviousZipCode from 'Components/Fields/PreviousZipCode';
 import PreviousCity from 'Components/Fields/PreviousCity';
 import MailingStreetAddress from 'Components/Fields/MailingStreetAddress';
@@ -38,7 +39,6 @@ function Addresses(props){
     const differentMailAddressField = fields.find(item => item.uuid === "e7340274-ee3f-4d73-a967-c9d7c249be7b");
     const mailCityField = fields.find(item => item.uuid === "9a5baee7-357b-4e59-b4f2-fe2525c0fd6c");
     const mailStateField = fields.find(item => item.uuid === "b0f80289-6084-4723-8278-110fda210f0d");
-    const mailZipcodeField = fields.find(item => item.uuid === "c4f9c0cb-2a25-4f1d-a93a-b06a19656cfe");
 
     const noAddressSection = fields.find(item => item.uuid === "3724c7cd-5ec7-4e3e-85cd-db0cab63e99b");
     const movedAndNoAddressSection = fields.find(item => item.uuid === "6dd20906-654e-427e-bb82-1e62aee9ed72");
@@ -186,34 +186,7 @@ function Addresses(props){
                             </Grid>
 
                             <Grid tablet={{ col: true }}>
-                            <div className="input-parent">
-                                <Label className="text-bold" htmlFor="mail-zip-code">
-                                    {mailZipcodeField.label} {(addressFieldsState.required === "1") && <span className={'required-text'}>*</span>}
-                                </Label>
-                                <span className="usa-hint" id="mail-zip-hint">{zipcodeField.help_text}</span>
-                                <TextInput
-                                    data-test="mailZip"
-                                    id="mail-zip-code"
-                                    className="radius-md"
-                                    aria-describedby="mail-zip-code_error"
-                                    name="mail-zip"
-                                    value={props.fieldData.mail_zip_code}
-                                    type="text"
-                                    inputMode="numeric"
-                                    autoComplete="off"
-                                    required={(parseInt(addressFieldsState.required))}
-                                    minLength={5}
-                                    maxLength={5}
-                                    onChange={props.saveFieldData('mail_zip_code')}
-                                    onKeyDown={(e) => restrictType(e, 'number')}
-                                    onBlur={(e) => toggleError(e, checkForErrors(e, 'check value length'))}
-                                    onInvalid={(e) => e.target.setCustomValidity(' ')}
-                                    onInput={(e) => e.target.setCustomValidity('')}
-                                />
-                                <span id="mail-zip-code_error" role="alert" className='error-text' data-test="errorText">
-                                    {mailZipcodeField.error_msg}
-                                </span>
-                            </div>
+                                <MailingZipCode {...props} />
                             </Grid>
                         </Grid>
                     </>
