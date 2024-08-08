17de0ff23f1dd913335fd441b0592a84cc511157
WilliamYKwon <william.kwon@gsa.gov>
2024-08-08 12:11:07 -0400

    VOTE-2386: implemented dynamic aria-describedby for sc to read hint & error (#260)

--- src/Components/FieldComponents/TextInputField.jsx
+++ src/Components/FieldComponents/TextInputField.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInput } from '@trussworks/react-uswds';
 import { restrictType, checkForErrors, toggleError } from 'Utils/ValidateField';
 
@@ -6,12 +6,17 @@ function TextInputField({ inputData, saveFieldData, fieldData }){
     const textType = inputData.type || 'text';
     const errorCheckType = inputData.check || 'check value exists';
 
+    
+    const hintId = inputData.id + '-hint';
+    const errorId = inputData.id + '_error';
+    const [a11yAnnounce, setA11yAnnounce] = useState(inputData.help_text ? hintId : errorId);
+
     return (
         <TextInput
         data-test={inputData.dataTest}
         id={inputData.id}
         className="radius-md"
-        aria-describedby={`${inputData.id}` + '_error'}
+        aria-describedby={a11yAnnounce}
         name={inputData.id}
         type={textType}
         autoComplete="off"
@@ -23,8 +28,8 @@ function TextInputField({ inputData, saveFieldData, fieldData }){
         onChange={saveFieldData(inputData.id)}
         onBlur={(e) => toggleError(e, checkForErrors(e, errorCheckType))}
         onKeyDown={(e) => restrictType(e, inputData.inputMode)}
-        onInvalid={(e) => e.target.setCustomValidity(' ')}
-        onInput={(e) => e.target.setCustomValidity('')}
+        onInvalid={(e) => {e.target.setCustomValidity(' '), setA11yAnnounce(errorId)}}
+        onInput={(e) => {e.target.setCustomValidity(''), setA11yAnnounce(hintId)}}
         />
     )
 }
