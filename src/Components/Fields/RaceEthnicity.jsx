ec6c211588c65c3c2a6af2d69f848cf06538be26
clmedders <christian.medders@gsa.gov>
2024-08-20 13:13:53 -0400

    move race and ethnicity to field container

---
+++ src/Components/Fields/RaceEthnicity.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import FieldContainer from 'Components/FieldContainer';
+import {getField} from "Utils/fieldParser";
+
+function RaceEthnicity(props){
+  const uuid = "2bfff6c6-6782-4b14-ac45-642efd278f6a";
+  const field = getField(props.fieldContent, uuid);
+  const stateField = getField(props.stateData.nvrf_fields, field.uuid);
+  return (
+    <FieldContainer
+      fieldType={'select'} inputData={{
+      id: 'race',
+      dataTest: 'select',
+      required: "0",
+      label: field.label,
+      options: field.options,
+      value: stateField.value,
+    }} saveFieldData={props.saveFieldData} fieldData={props.fieldData} stringContent={props.stringContent}/>
+  )
+}
+
+export default RaceEthnicity;
