8f989574bae5b092d41377e17ba4ad7e51b73a5d
SamiraMSadat <samira.sadat@gsa.gov>
2024-08-20 11:00:17 -0600

    VOTE-2363 Replace with json api path
    Conditionally use the locale in the path

--- src/Utils/JsonHelper.jsx
+++ src/Utils/JsonHelper.jsx
@@ -3,7 +3,7 @@ DOMPurify.setConfig({ADD_ATTR: ['target']});
 
 export const fetchData = async(filename, setContent) => {
     const locale = document.documentElement.lang;
-    const path = `data/${locale}/${filename}`;
+    const path = (locale == 'en') ? `api/nvrf/${filename}`: `${locale}/api/nvrf/${filename}`;
     const response = await fetch(path).then(response => response.json());
     setContent(response);
 }
