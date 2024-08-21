8143ad6c5d4b939e8bf4bc47fa1a1c2eff0ab693
SamiraMSadat <108487505+SamiraMSadat@users.noreply.github.com>
2024-08-21 17:05:42 -0600

    VOTE-2363 Replace with json api path (#285)

--- src/Utils/JsonHelper.jsx
+++ src/Utils/JsonHelper.jsx
@@ -2,8 +2,9 @@ import DOMPurify from "dompurify";
 DOMPurify.setConfig({ADD_ATTR: ['target']});
 
 export const fetchData = async(filename, setContent) => {
-    const locale = document.documentElement.lang;
-    const path = `data/${locale}/${filename}`;
+    const lang = document.documentElement.lang;
+    const locale = lang !== "en" ? `/${lang}` : "";
+    const path = `${locale}/nvrf/assets/${filename}`;
     const response = await fetch(path).then(response => response.json());
     setContent(response);
 }
