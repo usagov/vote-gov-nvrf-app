43c0fb896eae85ffbdfc9d9f4ee76f84fce09ff1
Ray Estrada <raymond.estrada@gsa.gov>
2024-08-21 15:09:30 -0700

    refinement

--- src/Utils/JsonHelper.jsx
+++ src/Utils/JsonHelper.jsx
@@ -2,9 +2,9 @@ import DOMPurify from "dompurify";
 DOMPurify.setConfig({ADD_ATTR: ['target']});
 
 export const fetchData = async(filename, setContent) => {
-    const locale = document.documentElement.lang;
-    const assetsPath = "/nvrf/assets/"
-    const path = (locale == 'en') ? `${assetsPath}${filename}`: `${locale}${assetsPath}${filename}`;
+    const lang = document.documentElement.lang;
+    const locale = lang !== "en" ? `/${lang}` : "";
+    const path = `${locale}/nvrf/assets/${filename}`;
     const response = await fetch(path).then(response => response.json());
     setContent(response);
 }
