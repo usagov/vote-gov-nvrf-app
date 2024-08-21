d9bb12ad4038efc672a3f92de43a4e4caaf15690
Ray Estrada <raymond.estrada@gsa.gov>
2024-08-21 15:30:57 -0700

    add base url

--- src/Utils/JsonHelper.jsx
+++ src/Utils/JsonHelper.jsx
@@ -3,7 +3,7 @@ DOMPurify.setConfig({ADD_ATTR: ['target']});
 
 export const fetchData = async(filename, setContent) => {
     const lang = document.documentElement.lang;
-    const locale = lang !== "en" ? `/${lang}` : "";
+    const locale = lang !== "en" ? lang : "";
     const path = `${locale}/nvrf/assets/${filename}`;
     const response = await fetch(path).then(response => response.json());
     setContent(response);
