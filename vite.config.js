6e20d8198a6833cd9d244e35a35d56b7c6a244c9
mlloydbixal <amanda.lloyd@gsa.gov>
2024-09-19 17:06:42 -0400

    add slash

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'BASEURL',
-  // base: 'https://vote.gov/foo/',
-  // Empty string or ./ (for embedded deployment)
   base: './',
   build: {
     rollupOptions: {
