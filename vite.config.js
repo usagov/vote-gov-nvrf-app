727d8e5d5476d83855572a87893aa145e4256328
mlloydbixal <amanda.lloyd@gsa.gov>
2024-09-19 16:27:18 -0400

    VOTE-2765: update pages script

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: 'BASEURL',
+  // base: 'https://vote.gov/foo/',
+  // Empty string or ./ (for embedded deployment)
   base: './',
   build: {
     rollupOptions: {
