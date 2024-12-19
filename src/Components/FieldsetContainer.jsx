7c4203011e1ac2812dd90b01a7fff83e4047c9fa
Ray Estrada <raymond.estrada@gsa.gov>
2024-12-19 12:21:24 -0800

    Update fieldset container for inclusion in step validation

--- src/Components/FieldsetContainer.jsx
+++ src/Components/FieldsetContainer.jsx
@@ -19,19 +19,28 @@ function FieldsetContainer({ fieldType, inputData, saveFieldData, dateFormat, fi
 
     return (
         <>
-            <Fieldset className={fieldError.length > 0 ? "vote-error-container" : "input-parent"} legend={inputData.required ? [<span key={0} className="text-bold">{inputData.label}</span>, <span key={1} className='required-text'>*</span>] : (inputData.stringContent)} style={{ marginTop: '30px' }}>
+            <div className={fieldError.length > 0 ? "vote-error-container" : "input-parent"}>
+                <Fieldset legend={inputData.required ? [<span key={0} className="text-bold">{inputData.label}</span>, <span key={1} className='required-text'>*</span>] : (inputData.stringContent)} style={{ marginTop: '30px' }}>
                     <span className="usa-hint" id={`${inputData.id} + '-hint'`}>
                         {inputData.help_text}
                     </span>
-                {renderField(fieldType)}
+                    {renderField(fieldType)}
 
-                {fieldError.map((error, index) => (
-                    <span id={`${error.id}` + '_error'} role="alert" key={index} className={'vote-error-text'}
-                          data-test="errorText">
+                    {fieldError.map((error, index) => (
+                        <span id={`${error.id}` + '_error'} role="alert" key={index} className={'vote-error-text'}
+                              data-test="errorText">
                             {error.message}
                         </span>
-                ))}
-            </Fieldset>
+                    ))}
+
+                    {fieldError.length === 0 && (
+                        <span id={`${inputData.id}` + '_error'} role="alert" className={'vote-error-text'}
+                              data-test="errorText">
+                        {inputData.error_msg}
+                    </span>
+                    )}
+                </Fieldset>
+            </div>
         </>
     )
 }
