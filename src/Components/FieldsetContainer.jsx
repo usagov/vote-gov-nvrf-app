06f2903e83e33e79074eb03e3e5bcc8c762ce1ef
SamiraMSadat <108487505+SamiraMSadat@users.noreply.github.com>
2024-12-31 12:09:32 -0700

    VOTE-3116 DOB validation updates (#348)
    * Vote-2828: Migrate NVRF to use page objects (#343)
    
    * Vote-2828: Update tests to use page objects
    
    * Vote-2828: removed unused tests
    
    * VOTE-2910 Replace Spanish PDF and revert workaround (#342)
    
    * VOTE-3116 Add new CMS fields for month, day, year
    
    * VOTE-3116 Put dob in its own grid, add field data for each dob subfield
    
    * VOTE-3070 touchpoints form spacing (#346)
    
    * Vote-135: NPM Audit (#347)
    
    * Stable updates
    
    * update vite
    
    * VOTE-3116 Remove toggle error from on blur for each dob field
    
    * refinements
    
    * Update fieldset container for inclusion in step validation
    
    * Update date of birth error test
    
    ---------
    
    Co-authored-by: Christian Medders - Bixal <88721460+clmedders@users.noreply.github.com>
    Co-authored-by: Ray Estrada <raymond.estrada@gsa.gov>

--- src/Components/FieldsetContainer.jsx
+++ src/Components/FieldsetContainer.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState} from "react";
 import { Fieldset } from '@trussworks/react-uswds';
 import DateFields from 'Components/FieldComponents/DateFields';
 
 function FieldsetContainer({ fieldType, inputData, saveFieldData, dateFormat, fieldData }) {
+    const [fieldError, setFieldError] = useState([]);
+
     function renderField(fieldType) {
         switch (fieldType) {
             case 'date':
@@ -10,21 +12,33 @@ function FieldsetContainer({ fieldType, inputData, saveFieldData, dateFormat, fi
                     inputData={inputData}
                     saveFieldData={saveFieldData}
                     dateFormat={dateFormat}
-                    fieldData={fieldData} />;
+                    fieldData={fieldData}
+                    setFieldError={setFieldError}/>;
         }
     };
 
     return (
         <>
-            <div className="input-parent">
-                <Fieldset className="fieldset" legend={inputData.required ? [<span key={0} className="text-bold">{inputData.label}</span>, <span key={1} className='required-text'>*</span>] : (inputData.stringContent)} style={{ marginTop: '30px' }}>
+            <div className={fieldError.length > 0 ? "vote-error-container" : "input-parent"}>
+                <Fieldset legend={inputData.required ? [<span key={0} className="text-bold">{inputData.label}</span>, <span key={1} className='required-text'>*</span>] : (inputData.stringContent)} style={{ marginTop: '30px' }}>
                     <span className="usa-hint" id={`${inputData.id} + '-hint'`}>
                         {inputData.help_text}
                     </span>
+
                     {renderField(fieldType)}
-                    <span id={`${inputData.id}` + '_error'} role="alert" aria-live="assertive" className={'vote-error-text'} data-test="errorText">
-                        {inputData.error_msg}
-                    </span>
+
+                    {fieldError.map((error, index) => (
+                        <span id={`${error.id}` + '_error'} role="alert" aria-live="assertive" key={index} className={'vote-error-text'}
+                              data-test="errorText">
+                            {error.message}
+                        </span>
+                    ))}
+
+                    {fieldError.length === 0 && (
+                        <span id={inputData.id + '_error'} role="alert" aria-live="assertive" className={'vote-error-text'} data-test="errorText">
+                            {inputData.error_msg}
+                        </span>
+                    )}
                 </Fieldset>
             </div>
         </>
