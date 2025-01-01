4d9ec9d3164c6b770d84384481610f41f3d28722
Ray Estrada <raymond.estrada@gsa.gov>
2024-12-31 17:12:16 -0800

    VOTE-920 migrate nvrf strings to cms

--- src/Components/Buttons/NextButton.jsx
+++ src/Components/Buttons/NextButton.jsx
@@ -7,7 +7,7 @@ function NextButton(props) {
       <Button className={'maxw-mobile-lg width-full tablet:width-auto'}
               data-test="nextBtn" type={props.type} onClick={props.onClick}>
         <span>{props.text}</span>
-        <Icon.ArrowForward aria-label={props.stringContent.forwardIcon}
+        <Icon.ArrowForward role="none" aria-hidden="true" alt=""
                            style={{margin: "-3px -3px -3px 4px"}}/>
       </Button>
     </>
