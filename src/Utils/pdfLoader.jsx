50d095aefff30f95591c3f0e190d4c4410cc3114
Ray Estrada <raymond.estrada@gsa.gov>
2025-02-06 15:30:41 -0800

    VOTE-3348 Adjust pdf location

--- src/Utils/pdfLoader.jsx
+++ src/Utils/pdfLoader.jsx
@@ -2,7 +2,7 @@ import {PDFDocument} from 'pdf-lib';
 
 const loadPdf = async () => {
   const lang = document.documentElement.lang;
-  const formUrl = `../files/Federal_Voter_Registration_${lang}.pdf`;
+  const formUrl = `${BASEURL}/files/Federal_Voter_Registration_${lang}.pdf`;
   const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
   const pdfDoc = await PDFDocument.load(formPdfBytes)
   const form = pdfDoc.getForm()
