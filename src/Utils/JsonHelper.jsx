601773701d8b1ce60be8b9f8c578db0752a6aad4
SamiraMSadat <samira.sadat@gsa.gov>
2025-02-25 11:07:49 -0700

    VOTE-336 Modify fetchData function to use cache as default

--- src/Utils/JsonHelper.jsx
+++ src/Utils/JsonHelper.jsx
@@ -1,16 +1,31 @@
 import DOMPurify from "dompurify";
+import { useState, useEffect } from "react";
 
 DOMPurify.setConfig({ADD_ATTR: ['target']});
 
 const lang = document.documentElement.lang;
 const locale = lang !== "en" ? `/${lang}` : '';
 
-export const fetchData = async (filename, setContent, setError) => {
-  const path = `${BASEURL}${locale}/nvrf/assets/${filename}`;
-  const response = await fetch(path)
+export function fetchData(filename, setContent, setError) {
+  const url = `${BASEURL}${locale}/nvrf/assets/${filename}`;
+  const cache = localStorage.getItem(url);
+
+  // set state from cache
+  if (cache) {
+    console.log("fetch from cache");
+    setContent(JSON.parse(cache));
+  } else {
+  // set state from api fetch
+  console.log("fetch from url");
+    fetch(url)
     .then(response => response.json())
+    .then(json => {
+      // set the json result in localStorage
+      localStorage.setItem(url, JSON.stringify(json));
+      setContent(json);
+    })
     .catch(() => setError(true));
-  setContent(response);
+  }
 }
 
 export const sanitizeDOM = (data) => DOMPurify.sanitize(data);
