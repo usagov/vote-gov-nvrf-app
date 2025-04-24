f59c84beaaac3d4a046ddb6b98ec1c7c9899edbf
Ray Estrada <raymond.estrada@gsa.gov>
2025-04-24 08:48:49 -0700

    Add default query cache timeouts

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,15 @@ import App from './App.jsx';
 import '@trussworks/react-uswds/lib/uswds.css';
 import '@trussworks/react-uswds/lib/index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // default stale time for every query (in milliseconds)
+      staleTime: 600000, // 10 minutes in milliseconds
+      cacheTime: 600000, // how long should it stay in the local cache
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
