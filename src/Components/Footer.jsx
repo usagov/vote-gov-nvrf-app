139e24cf8c1f05d08076af13b3f48f882926c738
Ray Estrada <raymond.estrada@gsa.gov>
2025-04-29 16:39:57 -0700

    Add conditional to footer content

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,12 +5,17 @@ import { sanitizeDOM } from 'Utils/JsonHelper';
 export default function Footer() {
   const { stateContent, stringContent } = useContext(DataContext);
   const lastUpdatedSanitized = sanitizeDOM(stateContent.data.nvrf_last_updated_date);
-  let cardFooter = stringContent.data.nvrf_footer.replace("@state_name", stateContent.data.name).replace("@date", lastUpdatedSanitized);
 
-  return (
-    <div
-      className="text-base margin-top-5 maxw-tablet margin-x-auto"
-      dangerouslySetInnerHTML={{__html: sanitizeDOM(cardFooter)}}
-    />
-  )
+  if (stringContent.data.nvrf_footer) {
+    let cardFooter = stringContent.data.nvrf_footer
+      .replace('@state_name', stateContent.data.name)
+      .replace('@date', lastUpdatedSanitized)
+
+    return (
+      <div
+        className="text-base margin-top-5 maxw-tablet margin-x-auto"
+        dangerouslySetInnerHTML={{ __html: sanitizeDOM(cardFooter) }}
+      />
+    )
+  }
 }
