5f8dee37f142d27188dd86012425b67c7dbb10e6
Ray Estrada <raymond.estrada@gsa.gov>
2025-06-04 15:01:49 -0700

    VOTE-3565 update location of NVRF pdfs

--- src/Utils/pdfLoader.jsx
+++ src/Utils/pdfLoader.jsx
@@ -1,10 +1,8 @@
 import { PDFDocument } from "pdf-lib";
 
-const BASE_URL = "https://vote.gov";
-
 const loadPdf = async () => {
   const lang = document.documentElement.lang;
-  const formUrl = `${BASE_URL}/files/Federal_Voter_Registration_${lang}.pdf`;
+  const formUrl = `/nvrf-assets/Federal_Voter_Registration_${lang}.pdf`;
   const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
   const pdfDoc = await PDFDocument.load(formPdfBytes);
   const form = pdfDoc.getForm();
